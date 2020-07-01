ab72b55dcbd2d8a6f5826bae3e809a4c7ca7ade2
Daniel <djpepin1299@gmail.com>
2020-07-01 17:33:02 -0400

    firstworkingsound

--- disbot.js
+++ disbot.js
@@ -1,2 +1,21 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
+const fetch = require("node-fetch");
+const dotenv = require("dotenv").config();
 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (message) => {
+  if (message.content === "ping ding") {
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      //dispatcher
+      const dispatcher = connection.play("media/WHAT A FUCKED UP DAY.mp3");
+     
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
