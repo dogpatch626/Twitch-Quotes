996881a573fbb722c8c1d35e0763e09fe0daef5c
Daniel <djpepin1299@gmail.com>
2020-07-05 15:15:37 -0400

    added end and disconnect

--- disbot.js
+++ disbot.js
@@ -3,21 +3,37 @@ const client = new Discord.Client();
 const fetch = require("node-fetch");
 const dotenv = require("dotenv").config();
 
+var dispatcher = client.voice.createBroadcast();
+var connection;
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//play handler
 client.on("message", async (message) => {
   if (message.content === "!ply") {
     if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
+      connection = await message.member.voice.channel.join();
       //dispatcher
-      const dispatcher = connection.play("media/WHAT A FUCKED UP DAY.mp3");
-     
+      dispatcher = connection.play("media/WHAT A FUCKED UP DAY.mp3");
+      dispatcher.on("finish", () => {
+        console.log("audio.mp3 has finished playing!");
+      });
     }
   }
 });
+//play handler
 
-dispatcher.destroy();
+client.on("message", (message) => {
+  if (message.content === "!end") {
+    dispatcher.end();
+  }
+});
 
+client.on("message", (message) => {
+  if (message.content === "!diss") {
+    connection = message.member.voice.channel.leave();
+    console.log("bot has joined channel");
+  }
+});
 client.login(process.env.BOT_TOKEN);
